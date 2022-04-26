6ce2787185a8feee2c7a11c3a285c190abfbd205
BlackKnightZERO <ayon.arif.10@gmail.com>
2022-04-26 11:09:47 -0400

    first commit

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,23 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <h1>Counter works everywhere</h1>
+    <h2 :style="{ color: store.state.colorCode }">{{
+      store.state.counter
+      }}</h2>
   </div>
 </template>
+
+<script>
+import { inject } from 'vue'
+
+export default{
+  setup() {
+    const store = inject('store')
+    
+    return {
+      store
+    }
+  }
+}
+</script>
+
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,91 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    
+    <div 
+      class="counter" 
+      :style="{ color: store.state.colorCode }"
+    >
+      {{ store.state.counter }}
+      <!-- counter -->
+    </div>
+    <div class="counter-squared">
+      {{ store.state.counter }}<sup>2</sup> = {{ store.getters.counterSquared() }}
+    </div>
+    
+    <div class="buttons">
+      <button @click="store.methods.increaseCounter">+</button>
+      <button @click="store.methods.decreaseCounter">-</button>
+      <!-- <button @click="increaseCounter">+</button> -->
+      <!-- <button @click="decreaseCounter">-</button> -->
+    </div>
+
+    <div>
+      <input
+        v-model="computedColorCode"
+        type="text"
+        placeholder="Enter color code">
+
+      <!-- <input
+        v-model="store.state.colorCode"
+        type="text"
+        placeholder="Enter color code"> -->
+    </div>
+
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+import { ref, inject, computed } from 'vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
+
+  setup() {
+
+    const store = inject('store')
+
+    const computedColorCode = computed({
+      get() {
+        return store.state.colorCode
+      },
+      set(val) {
+        store.methods.setColorCode(val)
+      }
+    })
+
+    // let counter = ref(0)
+
+    // const increaseCounter = () => {
+      // counter.value++
+    // }
+
+    // const decreaseCounter = () => {
+      // counter.value--
+    // }
+
+    // return { counter, increaseCounter, decreaseCounter }
+    
+    return { 
+      computedColorCode,
+      store 
+    }
+
   }
+
 }
 </script>
+
+<style scoped>
+  div {
+    margin-bottom:10px;
+  }
+  .counter {
+    font-size: 80px;
+  }
+  .buttons button {
+    font-size: 40px;
+    width: 100px;
+    margin: 0 10px;
+  }
+</style>
