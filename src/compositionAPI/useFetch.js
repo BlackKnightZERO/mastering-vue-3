bfee7c5a46fca6570032b54cdeddc2c26cff05dd
BlackKnightZERO <ayon.arif.10@gmail.com>
2022-04-27 10:41:38 -0400

    composition-api-useFetch-useFetchCache

--- src/compositionAPI/useFetch.js
+++ src/compositionAPI/useFetch.js
@@ -39,5 +39,24 @@ export const useFetch = (url, config = {}) => {
 export const useFetchCache = (key, url, config) => {
     const info = useFetch(url, { skip: true, ...config})
 
+    const update = () => cacheMap.set(key, info.response.value)
+    const clear = () => cacheMap.set(key, undefined)
+
+    const fetch = async () => {
+        try {
+            await info.fetch()
+            update()
+        } catch {
+            clear()
+        }
+    }
+
+    const response = computed(() => cacheMap.get(key))
+    const data = computed(() => response.value?.data)
+
+    if( response.value ==null ) fetch()
+
+    return { ...info, fetch, data, response, clear }
+
     
 }
